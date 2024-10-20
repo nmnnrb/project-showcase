c099423aaac5ae0bd43260eee420022d232cb118
nmnnrb <151563433+nmnnrb@users.noreply.github.com>
2024-10-21 00:54:50 +0530

    latest-resume-butto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,8 @@ function footer() {
             
             </div>
             <div className='basis-1/3 flex flex-col items-center justify-end '>
-            <p className='text-right ml-3 mt-6 text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consequuntur dolorem numquam quod iure eveniet in corporis! Voluptate.</p>
-            <a href="https://www.linkedin.com/in/nmnnrb/"> <img className='mt-10 ml-1   w-32 h-6' src='/botton.png' /></a>
+            <p className='text-right ml-3 mt-6 text-xs'>This is just a showcase or the showing of my project in a interactive pattern you can always visit my latest resume by click the <a href="http://bit.ly/Namanportfolio">below button</a> .</p>
+            <a href="http://bit.ly/Namanportfolio"> <img className='mt-10 ml-1   w-32 h-6' src='/botton.png' /></a>
             </div>
             </div>
         </div>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ import Button from './Button'
 function Product({data , mover ,count , bgColor}) {
   const [bgColorr, setBgColorr] = useState("transparent");
   return (
-    <div className={`w-full hover:bg-[${bgColor}]  text-white`}   style={{ backgroundColor: bgColorr,transition: "background-color 0.3s Ease-In-Out" }} onMouseEnter={() => {
+    <div className={`w-full hover:bg-[${bgColor}]   text-white`}   style={{ backgroundColor: bgColorr,transition: "background-color 0.3s Ease-In-Out" }} onMouseEnter={() => {
       setBgColorr(bgColor); // Set background color on hover
     }}
     onMouseLeave={() => setBgColorr("transparent")} // Reset on mouse leave
@@ -12,11 +12,11 @@ function Product({data , mover ,count , bgColor}) {
         <div onMouseEnter={() => {mover(count)} } key={count} className="max-w-screen-xl  md:h-[23rem]   mx-auto flex flex-col md:flex-row my-44 md:my-0 p-2 md:items-center md:justify-between">
           <div className="flex flex-wrap md:w-1/3"><h1 className='font-medium capitalize mb-6 md:mb-0 text-6xl' >{data.title}</h1></div>
             
-            <div className="dets md:w-1/3 flex flex-col  ">
-                <p className='mb-10 text-zinc-200 text-xs w-fit md:text-sm' >{data.description}</p>
+            <div className="dets md:w-1/3 flex  flex-col  ">
+                <p className='mb-10 text-zinc-200  text-xs w-fit md:text-sm' >{data.description}</p>
                 <div className="flex gap-5">
                 <p className='font-normal text-zinc-400 text-sm' ><b className='font-extrabold text-md mr-1' >Skills</b> {data.skills} </p>
-                <p className='font-normal text-zinc-100 mb-12 text-sm' ><b className='font-extrabold text-md mr-1' >Key</b> {data.key} </p>
+                <p className='font-normal text-zinc-100 mb-4 text-sm' ><b className='font-extrabold text-md mr-1' >Key</b> {data.key} </p>
                 </div>
                 <div className="flex items-center gap-4 justify-end">
                 {data.live && <Button click={data.livelink} title="Live Link"/>}
